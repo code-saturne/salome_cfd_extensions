code-saturne/salome_cfd_extensions

commit 27382c9fdd931a9e64e171ffc6cd63f23235a9a4

Update cfd_study following commit b7f67685 of code_saturne

cfd_study/src/CFDSTUDYGUI/CFDSTUDYGUI_Commons.py
@@ -59,6 +59,9 @@ sg = libSALOME_Swig.SALOMEGUI_Swig()
 CFD_Saturne = "Code_Saturne"
 CFD_Neptune = "NEPTUNE_CFD"
 
+_CFD_solvers = {CFD_Saturne:'code_saturne',
+                CFD_Neptune:'neptune_cfd'}
+
 # ObjectTR is a convenient object for traduction purpose
 ObjectTR = QObject()
 
@@ -93,6 +96,20 @@ def CFD_Code():
     global _CFD_Code
     return _CFD_Code
 
+def getCFDSolverName(code_name=None):
+    """
+    Get cfd solver name.
+    If 'code_name' is not provided, default value (CFD_Code()) is used.
+    """
+    solver_name = None
+
+    if code_name == None:
+        code_name = CFD_Code()
+
+    if code_name in _CFD_solvers.keys():
+        solver_name = CFD_solvers[code_name]
+
+    return solver_name
 
 def _SetCFDCode(var):
     log.debug("_SetCFDCode : var = %s" % var)
@@ -135,33 +152,34 @@ def CheckCFD_CodeEnv(code):
         iok= False
         return iok,mess
 
-    if code == CFD_Saturne:
+    else:
+        iok = False
+        _solver_name = getCFDSolverName(code);
         try:
             from code_saturne.cs_package import package
-            iok = True
-        except ImportError as e:
+            pkg = package(name = _solver_name)
+            b = os.path.join(pkg.get_dir('bindir'),
+                             _solver_name+pkg.config.shext)
+            if os.path.isfile(b):
+                iok = True
+            else:
+                mess = cfdstudyMess.trMessage(ObjectTR.tr("INFO_DLG_INVALID_ENV"),[code]) + e.__str__()
+                mess = mess + cfdstudyMess.trMessage(ObjectTR.tr("CHECK_CODE_INSTALLATION"),[_solver_name,code])
+
+        except:
             mess = cfdstudyMess.trMessage(ObjectTR.tr("INFO_DLG_INVALID_ENV"),[code]) + e.__str__()
             if "cs_package" in e.__str__():
                 mess = mess + cfdstudyMess.trMessage(ObjectTR.tr("CHECK_CODE_PACKAGE"),["cs_package",code])
-            elif "code_saturne" in e.__str__():
-                mess = mess + cfdstudyMess.trMessage(ObjectTR.tr("CHECK_PYTHON_PATH"),[])
-            iok = False
-    elif code == CFD_Neptune:
-        try:
-            from neptune_cfd.nc_package import package
-            iok = True
-        except ImportError as e:
-            mess = cfdstudyMess.trMessage(ObjectTR.tr("INFO_DLG_INVALID_ENV"),[code]) + e.__str__()
-            if "nc_package" in e.__str__():
-                mess = mess + cfdstudyMess.trMessage(ObjectTR.tr("CHECK_CODE_PACKAGE"),["nc_package",code])
-            elif "neptune_cfd" in e.__str__():
-                mess = mess + cfdstudyMess.trMessage(ObjectTR.tr("CHECK_PYTHON_PATH"),[])
-            iok = False
+            elif _solver_name in e.__str__():
+                mess = mess + cfdstudyMess.trMessage(ObjectTR.tr("CHECK_CODE_PACKAGE"),[_solver_name,code])
+
+
     else:
         raise ValueError("Invalid name of solver!")
 
     if iok:
-        pkg = package()
+        _solver_name = getCFDSolverName(code);
+        pkg = package(name = _solver_name)
         prefix = pkg.get_dir('prefix')
         log.debug("CheckCFD_CodeEnv -> prefix = %s" % (prefix))
 
@@ -187,16 +205,16 @@ def BinCode():
     mess = ""
     # default package is code_saturne (for convert...)
     from code_saturne.cs_package import package
-    pkg = package()
 
-    if CFD_Code() == CFD_Saturne:
-        bindir = pkg.get_dir('bindir')
-        b = os.path.join(bindir, "code_saturne")
-    elif CFD_Code() == CFD_Neptune:
-        from neptune_cfd.nc_package import package
-        pkg = package()
-        bindir = pkg.get_dir('bindir')
-        b = os.path.join(bindir, "neptune_cfd")
+    try:
+        _solver_name = getCFDSolverName()
+    except:
+        raise Exception("Uknown CFD solver: '%s'" % str(CFD_code()))
+
+    pkg = package(name=_solver_name)
+
+    b = os.path.join(pkg.get_dir('bindir'),
+                     _solver_name+pkg.config.shext)
     c = pkg.get_preprocessor()
     log.debug("BinCode -> \n    %s\n    %s" % (b, c))
     return b, c, mess

cfd_study/src/CFDSTUDYGUI/CFDSTUDYGUI_ActionsHandler.py
@@ -914,9 +914,12 @@ class CFDSTUDYGUI_ActionsHandler(QObject):
             if a != SolverFileMenu and a != SolverToolsMenu and a != SolverHelpMenu:
                 self.solverAction(a).setEnabled(isActivatedView)
 
-        try:
-            from neptune_cfd.nc_package import package
-        except:
+        from code_saturne.cs_package import package
+        pkg = package(name='neptune_cfd')
+        bin_ncfd = os.path.join(pkg.get_dir('bindir'),
+                                'neptune_cfd'+pkg.config.shext)
+
+        if not os.path.isfile(bin_ncfd):
             self.solverAction(NCSolverHelpUserGuide).setEnabled(False)
             self.solverAction(NCSolverHelpTutorial).setEnabled(False)
             self.solverAction(NCSolverHelpTheory).setEnabled(False)

cfd_study/src/CFDSTUDYGUI/CFDSTUDYGUI_DialogCollector.py
@@ -59,7 +59,7 @@ from ui_ECSConversionDialog   import Ui_ECSConversionDialog
 from ui_GUIActivationDialog   import Ui_GUIActivationDialog
 import CFDSTUDYGUI_DataModel
 import CFDSTUDYGUI_Commons
-from CFDSTUDYGUI_Commons import _SetCFDCode, CFD_Code, sgPyQt
+from CFDSTUDYGUI_Commons import _SetCFDCode, CFD_Code, getCFDSolverName, sgPyQt
 from CFDSTUDYGUI_Commons import CFD_Saturne, CFD_Neptune, CheckCFD_CodeEnv
 from CFDSTUDYGUI_Message import cfdstudyMess
 #-------------------------------------------------------------------------------
@@ -116,31 +116,32 @@ class InfoDialogHandler(InfoDialog):
 
 
     def setCode(self, env_saturne, env_neptune):
+
+        from code_saturne.cs_package import package
+
         if env_neptune:
             code = CFD_Neptune
-            from neptune_cfd.nc_package import package
-
         elif env_saturne:
             code = CFD_Saturne
-            from code_saturne.cs_package import package
-
         else:
             raise ValueError("Invalid CFD_Code in InfoDialog class")
 
-        pkg = package()
+        _solver_name = getCFDSolverName(code)
+        pkg = package(name = _solver_name)
+
         self.labelVersionValue.setText(pkg.version)
         self.labelPrefixValue.setText(pkg.get_dir('prefix'))
         self.labelCodeValue.setText(pkg.name)
+
         _SetCFDCode(code)
 
 
     def update(self, code):
-        if code == CFD_Saturne:
-            from code_saturne.cs_package import package
-        if code == CFD_Neptune:
-            from neptune_cfd.nc_package import package
+        from code_saturne.cs_package import package
 
-        pkg = package()
+        _solver_name = getCFDSolverName(code)
+        pkg = package(name = _solver_name)
+
         self.labelVersionValue.setText(pkg.version)
         self.labelPrefixValue.setText(pkg.get_dir('prefix'))
         self.labelCodeValue.setText(pkg.name)

cfd_study/src/CFDSTUDYGUI/CFDSTUDYGUI_SolverGUI.py
@@ -53,7 +53,7 @@ from code_saturne.Base.QtWidgets import *
 # Application modules
 #-------------------------------------------------------------------------------
 
-from CFDSTUDYGUI_Commons import CFD_Code, CFD_Saturne, CFD_Neptune, sgPyQt, sg
+from CFDSTUDYGUI_Commons import CFD_Code, CFD_Saturne, CFD_Neptune, getCFDSolverName, sgPyQt, sg
 from CFDSTUDYGUI_Commons import LoggingMgr
 import CFDSTUDYGUI_DataModel
 from CFDSTUDYGUI_Management import CFDGUI_Management
@@ -340,27 +340,28 @@ class CFDSTUDYGUI_SolverGUI(QObject):
 
 
     def onNeptuneHelpManual(self):
-        from neptune_cfd.nc_package import package
+        from code_saturne.cs_package import package
         argv_info = ['--guide', 'user']
-        cs_info.main(argv_info, package())
+        cs_info.main(argv_info, package(name='neptune_cfd'))
 
 
     def onNeptuneHelpTutorial(self):
-        from neptune_cfd.nc_package import package
+        from code_saturne.cs_package import package
         argv_info = ['--guide', 'tutorial']
-        cs_info.main(argv_info, package())
+        cs_info.main(argv_info, package(name='neptune_cfd'))
 
 
     def onNeptuneHelpKernel(self):
-        from neptune_cfd.nc_package import package
+        from code_saturne.cs_package import package
         argv_info = ['--guide', 'theory']
-        cs_info.main(argv_info, package())
+        cs_info.main(argv_info, package(name='neptune_cfd'))
 
 
     def onNeptuneHelpDoxygen(self):
+        from code_saturne.cs_package import package
         from neptune_cfd.nc_package import package
         argv_info = ['--guide', 'Doxygen']
-        cs_info.main(argv_info, package())
+        cs_info.main(argv_info, package(name='neptune_cfd'))
 
 
     def setWindowTitle_CFD(self,mw,aCase,baseTitleName):
@@ -379,10 +380,7 @@ class CFDSTUDYGUI_SolverGUI(QObject):
         log.debug("launchGUI")
         from code_saturne.cs_gui import process_cmd_line
         from code_saturne.Base.MainView import MainView
-        if CFD_Code() == CFD_Saturne:
-            from code_saturne.cs_package import package
-        elif CFD_Code() == CFD_Neptune:
-            from neptune_cfd.nc_package import package
+        from code_saturne.cs_package import package
 
         if sobjXML == None:
             Title = "unnamed"
@@ -390,7 +388,11 @@ class CFDSTUDYGUI_SolverGUI(QObject):
             Title = sobjXML.GetName()
 
         self.Workspace = WorkSpace
-        pkg = package()
+
+        # Get current solver name
+        _solver_name = getCFDSolverName()
+        pkg = package(name = _solver_name)
+
         case, splash = process_cmd_line(Args)
         try:
             mw = MainView(pkg, case, aCase)

cfd_study/src/CFDSTUDYOTURNS/CFDSTUDYOTURNS_StudyInterface.py
@@ -231,17 +231,14 @@ class cfd_openturns_study:
         """
         Setting the package according to the requested code by the user
         """
+        from code_saturne.cs_package import package
 
         pkg_name = self.cfg.get('study_parameters', 'code_name')
 
-        if pkg_name == 'code_saturne':
-            from code_saturne.cs_package import package
-        elif pkg_name == 'neptune_cfd':
-            from neptune_cfd.nc_package import package
-        else:
+        if pkg_name not in ['code_saturne', 'neptune_cfd']:
             raise Exception("Uknown package: "+pkg_name)
 
-        self.pkg = package()
+        self.pkg = package(name = pkg_name)
 
     # ---------------------------------------
 

commit 9e8b0ec208289e84df0921e4fc0530f2cfe3434d

Fix errors introduced by commit 27382c9f (March 16 2021).

cfd_study/src/CFDSTUDYGUI/CFDSTUDYGUI_Commons.py
@@ -107,7 +107,7 @@ def getCFDSolverName(code_name=None):
         code_name = CFD_Code()
 
     if code_name in _CFD_solvers.keys():
-        solver_name = CFD_solvers[code_name]
+        solver_name = _CFD_solvers[code_name]
 
     return solver_name
 
@@ -148,9 +148,10 @@ def CheckCFD_CodeEnv(code):
     bindir = ""
 
     if code not in [CFD_Saturne, CFD_Neptune]:
-        mess = cfdstudyMess.trMessage(ObjectTR.tr("CFDSTUDY_INVALID_SOLVER_NAME"),[code,CFD_Saturne,CFD_Neptune])
+        mess = cfdstudyMess.trMessage(ObjectTR.tr("CFDSTUDY_INVALID_SOLVER_NAME"),
+                                      [code,CFD_Saturne,CFD_Neptune])
         iok= False
-        return iok,mess
+        return iok, mess
 
     else:
         iok = False
@@ -163,19 +164,20 @@ def CheckCFD_CodeEnv(code):
             if os.path.isfile(b):
                 iok = True
             else:
-                mess = cfdstudyMess.trMessage(ObjectTR.tr("INFO_DLG_INVALID_ENV"),[code]) + e.__str__()
-                mess = mess + cfdstudyMess.trMessage(ObjectTR.tr("CHECK_CODE_INSTALLATION"),[_solver_name,code])
+                mess = cfdstudyMess.trMessage(ObjectTR.tr("INFO_DLG_INVALID_ENV"),
+                                              [code]) + e.__str__()
+                mess += cfdstudyMess.trMessage(ObjectTR.tr("CHECK_CODE_INSTALLATION"),
+                                               [_solver_name,code])
 
         except:
-            mess = cfdstudyMess.trMessage(ObjectTR.tr("INFO_DLG_INVALID_ENV"),[code]) + e.__str__()
+            mess = cfdstudyMess.trMessage(ObjectTR.tr("INFO_DLG_INVALID_ENV"),
+                                          [code]) + e.__str__()
             if "cs_package" in e.__str__():
-                mess = mess + cfdstudyMess.trMessage(ObjectTR.tr("CHECK_CODE_PACKAGE"),["cs_package",code])
+                mess += cfdstudyMess.trMessage(ObjectTR.tr("CHECK_CODE_PACKAGE"),
+                                               ["cs_package",code])
             elif _solver_name in e.__str__():
-                mess = mess + cfdstudyMess.trMessage(ObjectTR.tr("CHECK_CODE_PACKAGE"),[_solver_name,code])
-
-
-    else:
-        raise ValueError("Invalid name of solver!")
+                mess += cfdstudyMess.trMessage(ObjectTR.tr("CHECK_CODE_PACKAGE"),
+                                               [_solver_name,code])
 
     if iok:
         _solver_name = getCFDSolverName(code);
@@ -187,11 +189,13 @@ def CheckCFD_CodeEnv(code):
         log.debug("CheckCFD_CodeEnv -> prefix = %s" % (bindir))
 
         if not os.path.exists(prefix):
-            mess = cfdstudyMess.trMessage(ObjectTR.tr("ENV_DLG_INVALID_DIRECTORY"),[prefix])
+            mess = cfdstudyMess.trMessage(ObjectTR.tr("ENV_DLG_INVALID_DIRECTORY"),
+                                          [prefix])
             iok = False
         else:
             if not os.path.exists(bindir):
-                mess = cfdstudyMess.trMessage(ObjectTR.tr("ENV_DLG_INVALID_DIRECTORY"),[bindir])
+                mess = cfdstudyMess.trMessage(ObjectTR.tr("ENV_DLG_INVALID_DIRECTORY"),
+                                              [bindir])
                 iok = False
 
     log.debug("CheckCFD_CodeEnv -> %s = %s" % (code, iok))